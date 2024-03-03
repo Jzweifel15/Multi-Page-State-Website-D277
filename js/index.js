9e78a3eed03ceadd7d8d5af93cffab42c93b8995
Justin R. Zweifel <47803568+Jzweifel15@users.noreply.github.com>
2024-03-03 01:05:35 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() { 
+
+    document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.menu').classList.toggle('show'));
+    
+});
