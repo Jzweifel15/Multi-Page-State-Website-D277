a6e97cf73d3da47b3735d1d76d9e86d0c937a0cf
Justin R. Zweifel <47803568+Jzweifel15@users.noreply.github.com>
2024-03-10 03:33:35 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() { 
 
+    // Toggles classes for the hamburger button in the header
     document.querySelector(".menu-btn").addEventListener("click", () => {
         document.querySelector(".menu-btn").classList.toggle("pressed");
         document.querySelector(".menu").classList.toggle("show");
